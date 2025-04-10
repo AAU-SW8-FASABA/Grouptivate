bf81de63f059a9929fdefcb2f80027177976264c
NybyDK <nyby.dev@gmail.com>
2025-04-10 14:38:10 +0200

    more universal sizing

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -45,14 +45,14 @@ export default function TabLayout() {
         name="index"
         options={{
           title: 'Home',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="house.fill" color={color} />,
+          tabBarIcon: ({ color }) => <IconSymbol size={32} name="house.fill" color={color} />,
         }}
       />
       <Tabs.Screen
         name="profile"
         options={{
           title: 'Profile',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="person.fill" color={color} />,
+          tabBarIcon: ({ color }) => <IconSymbol size={32} name="person.fill" color={color} />,
         }}
       />
     </Tabs>
