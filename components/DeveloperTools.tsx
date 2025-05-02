2a85b96a8fb81cb1d766bb067644e467653809b0
Anton <dev@atjn.dk>
2025-05-02 13:57:03 +0200

    Refactor profile (#65)

--- components/DeveloperTools.tsx
+++ components/DeveloperTools.tsx
@@ -44,129 +44,139 @@ export function DeveloperTools() {
   const healthAdapterPromise = getHealthAdapter();
 
   return __DEV__ ? (
-    <View>
-      <Container>
-        <Text style={[styles.headerText]}>Perform activity</Text>
-        <Text style={[styles.text, { fontSize: 20, marginTop: 10 }]}>
-          Activity
-        </Text>
-        <Dropdown
-          style={[styles.dropdown, isActivityFocus && { borderColor: "blue" }]}
-          placeholderStyle={[styles.text, { fontSize: 20 }]}
-          selectedTextStyle={[styles.text, { fontSize: 20 }]}
-          itemTextStyle={[styles.text, { fontSize: 20 }]}
-          data={activities}
-          labelField="label"
-          valueField="value"
-          placeholder="Select"
-          onFocus={() => setIsActivityFocus(true)}
-          onBlur={() => setIsActivityFocus(false)}
-          value={activityValue}
-          onChange={(item) => {
-            setActivityValue(item.value);
-            setIsActivityFocus(false);
-          }}
-          renderRightIcon={() => (
-            <UniversalIcon
-              source={IconSource.FontAwesome6}
-              name="chevron-down"
-              size={20}
-              color="black"
-              style={{
-                transform: [{ rotate: isActivityFocus ? "180deg" : "0deg" }],
-                marginRight: 5,
-              }}
-            />
-          )}
-        />
-        <Text style={[styles.text, { fontSize: 20, marginTop: 10 }]}>
-          Metric
-        </Text>
-        <Dropdown
-          style={[styles.dropdown, isMetricFocus && { borderColor: "blue" }]}
-          placeholderStyle={[styles.text, { fontSize: 20 }]}
-          selectedTextStyle={[styles.text, { fontSize: 20 }]}
-          itemTextStyle={[styles.text, { fontSize: 20 }]}
-          data={metrics}
-          labelField="label"
-          valueField="value"
-          placeholder="Select"
-          onFocus={() => setIsMetricFocus(true)}
-          onBlur={() => setIsMetricFocus(false)}
-          value={metricValue}
-          onChange={(item) => {
-            setMetricValue(item.value);
-            setIsMetricFocus(false);
-          }}
-          renderRightIcon={() => (
-            <UniversalIcon
-              source={IconSource.FontAwesome6}
-              name="chevron-down"
-              size={20}
-              color="black"
-              style={{
-                transform: [{ rotate: isMetricFocus ? "180deg" : "0deg" }],
-                marginRight: 5,
-              }}
-            />
-          )}
-        />
-        <Text style={[styles.text, { fontSize: 20, marginTop: 10 }]}>
-          Amount
-        </Text>
-        <TextInput
-          style={globalStyles.inputField}
-          value={String(amountValue)}
-          onChangeText={(text) => setAmountValue(Number(text))}
-        ></TextInput>
-        <View style={{ width: "50%" }}>
-          <TouchableOpacity
-            style={{ marginTop: 10, marginBottom: 5 }}
-            onPress={async () => {
-              const healthAdapter = await healthAdapterPromise;
-              const options = {
-                startDate: new Date(Date.now() - 1000),
-                endDate: new Date(),
-                activity: activityValue,
-              };
-              if (isSportInsertOptions(options)) {
-                if (metricValue === Metric.Distance) {
-                  options.distance = amountValue;
-                  healthAdapter?.insertData(options);
-                } else if (metricValue === Metric.Calories) {
-                  options.caloriesBurned = amountValue;
-                  healthAdapter?.insertData(options);
-                } else {
-                  console.error(
-                    "This activity only supports distance and calories",
-                  );
-                }
-              } else if (isCountOnlyInsertOptions(options)) {
-                if (metricValue === Metric.Count) {
-                  options.count = amountValue;
-                  healthAdapter?.insertData(options);
-                } else {
-                  console.error("This activity only supports count");
-                }
-              } else {
-                console.error("Unexpected activity type");
-              }
-              SyncActivity();
+    <>
+      <Text
+        style={[styles.text, { fontSize: 32, marginTop: 50, marginBottom: 10 }]}
+      >
+        Developer tools
+      </Text>
+      <View>
+        <Container>
+          <Text style={[styles.headerText]}>Perform activity</Text>
+          <Text style={[styles.text, { fontSize: 20, marginTop: 10 }]}>
+            Activity
+          </Text>
+          <Dropdown
+            style={[
+              styles.dropdown,
+              isActivityFocus && { borderColor: "blue" },
+            ]}
+            placeholderStyle={[styles.text, { fontSize: 20 }]}
+            selectedTextStyle={[styles.text, { fontSize: 20 }]}
+            itemTextStyle={[styles.text, { fontSize: 20 }]}
+            data={activities}
+            labelField="label"
+            valueField="value"
+            placeholder="Select"
+            onFocus={() => setIsActivityFocus(true)}
+            onBlur={() => setIsActivityFocus(false)}
+            value={activityValue}
+            onChange={(item) => {
+              setActivityValue(item.value);
+              setIsActivityFocus(false);
             }}
-          >
-            <Text
-              style={[
-                styles.text,
-                styles.button,
-                { backgroundColor: "#57A773", color: "white" },
-              ]}
+            renderRightIcon={() => (
+              <UniversalIcon
+                source={IconSource.FontAwesome6}
+                name="chevron-down"
+                size={20}
+                color="black"
+                style={{
+                  transform: [{ rotate: isActivityFocus ? "180deg" : "0deg" }],
+                  marginRight: 5,
+                }}
+              />
+            )}
+          />
+          <Text style={[styles.text, { fontSize: 20, marginTop: 10 }]}>
+            Metric
+          </Text>
+          <Dropdown
+            style={[styles.dropdown, isMetricFocus && { borderColor: "blue" }]}
+            placeholderStyle={[styles.text, { fontSize: 20 }]}
+            selectedTextStyle={[styles.text, { fontSize: 20 }]}
+            itemTextStyle={[styles.text, { fontSize: 20 }]}
+            data={metrics}
+            labelField="label"
+            valueField="value"
+            placeholder="Select"
+            onFocus={() => setIsMetricFocus(true)}
+            onBlur={() => setIsMetricFocus(false)}
+            value={metricValue}
+            onChange={(item) => {
+              setMetricValue(item.value);
+              setIsMetricFocus(false);
+            }}
+            renderRightIcon={() => (
+              <UniversalIcon
+                source={IconSource.FontAwesome6}
+                name="chevron-down"
+                size={20}
+                color="black"
+                style={{
+                  transform: [{ rotate: isMetricFocus ? "180deg" : "0deg" }],
+                  marginRight: 5,
+                }}
+              />
+            )}
+          />
+          <Text style={[styles.text, { fontSize: 20, marginTop: 10 }]}>
+            Amount
+          </Text>
+          <TextInput
+            style={globalStyles.inputField}
+            value={String(amountValue)}
+            onChangeText={(text) => setAmountValue(Number(text))}
+          ></TextInput>
+          <View style={{ width: "50%" }}>
+            <TouchableOpacity
+              style={{ marginTop: 10, marginBottom: 5 }}
+              onPress={async () => {
+                const healthAdapter = await healthAdapterPromise;
+                const options = {
+                  startDate: new Date(Date.now() - 1000),
+                  endDate: new Date(),
+                  activity: activityValue,
+                };
+                if (isSportInsertOptions(options)) {
+                  if (metricValue === Metric.Distance) {
+                    options.distance = amountValue;
+                    healthAdapter?.insertData(options);
+                  } else if (metricValue === Metric.Calories) {
+                    options.caloriesBurned = amountValue;
+                    healthAdapter?.insertData(options);
+                  } else {
+                    console.error(
+                      "This activity only supports distance and calories",
+                    );
+                  }
+                } else if (isCountOnlyInsertOptions(options)) {
+                  if (metricValue === Metric.Count) {
+                    options.count = amountValue;
+                    healthAdapter?.insertData(options);
+                  } else {
+                    console.error("This activity only supports count");
+                  }
+                } else {
+                  console.error("Unexpected activity type");
+                }
+                SyncActivity();
+              }}
             >
-              Exercise!
-            </Text>
-          </TouchableOpacity>
-        </View>
-      </Container>
-    </View>
+              <Text
+                style={[
+                  styles.text,
+                  styles.button,
+                  { backgroundColor: "#57A773", color: "white" },
+                ]}
+              >
+                Exercise!
+              </Text>
+            </TouchableOpacity>
+          </View>
+        </Container>
+      </View>
+    </>
   ) : null;
 }
 
